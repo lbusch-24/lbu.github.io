06c895b15595217c30c0e8b96a4f53297c08418c
Laura Busch <lbusch@Lauras-MacBook-Pro.local>
2026-09-04 11:20:11 +0200

    Update

--- build/browsersync.js
+++ build/browsersync.js
@@ -39,8 +39,14 @@ module.exports = gulp => {
     'serve',
     gulp.series('jekyll-dev', () => {
       browserSync.init({
+        host: '0.0.0.0',
         server: {
           baseDir: '_site',
+        },
+        callbacks: {
+          ready: function (err, bs) {
+            console.log('Open on your phone: http://' + bs.options.get('urls').get('external') + ':3000');
+          },
         },
       });
 
